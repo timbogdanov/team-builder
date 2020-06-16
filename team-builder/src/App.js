5ff74c8573a8982bca0e0b0bc374942cb691d94d
Timofey Bogdanov <timofeybogdanov@Timofeys-iMac.local>
2020-06-16 12:29:48 -0700

    add initial state to memberList

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -3,8 +3,23 @@ import './App.css';
 
 function App() {
 
-  const [memberList, setMemberList] = useState();
+  const initialFriendsList = [
+    {
+      name: 'Tim',
+      email: 'tim@time.com',
+      role: 'Doctor',
+    },
+    {
+      name: 'Mark',
+      email: 'mark@mark.com',
+      role: 'Professional Truck Driver',
+    },
+  ]
 
+  
+  const [memberList, setMemberList] = useState(initialFriendsList);
+  
+  console.log(memberList)
   return (
     <div className="App">
 
